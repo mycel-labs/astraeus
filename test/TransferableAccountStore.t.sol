be878f31fd600117abb5f0a8d31c2a7eb158746a
peaceandwhisky <pioneerandf@gmail.com>
2024-08-15 10:53:50 +0900

    delete testGenerateEthereumAddress

--- test/TransferableAccountStore.t.sol
+++ test/TransferableAccountStore.t.sol
@@ -28,11 +28,4 @@ contract TestContract is Test {
             hex"a34b99f22c790c4e36b2b3c2c35a36db06226e41c692fc82b8b56ac1c540c5bd5b8dec5235a0fa8722476c7709c02559e3aa73aa03918ba2d492eea75abea235";
         assertBytesEq(publicKey, expectedPublicKey);
     }
-
-    function testGenerateEthereumAddress() public view {
-        address generatedAddress = c.generateEthereumAddress(privateKey);
-
-        address expectedAddress = 0x41aD2bc63A2059f9b623533d87fe99887D794847;
-        assertEq(generatedAddress, expectedAddress);
-    }
 }
