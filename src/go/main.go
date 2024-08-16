9d79f241bca43954aa73e6d647d97b2e97932f06
foxytanuki <biz@foxytanuki.dev>
2024-08-16 12:33:58 +0900

    add commented out empty functions to main.go

--- src/go/main.go
+++ src/go/main.go
@@ -17,7 +17,57 @@ type server struct {
 	pb.UnimplementedAccountServiceServer
 }
 
-// Add implementations for each AccountService method here
+// func (s *server) CreateAccount(ctx context.Context, req *pb.CreateAccountRequest) (*pb.BytesResponse, error) {
+// 	return &pb.BytesResponse{}, nil
+// }
+
+// func (s *server) TransferAccount(ctx context.Context, req *pb.TransferAccountRequest) (*pb.BytesResponse, error) {
+// 	return &pb.BytesResponse{}, nil
+// }
+
+// func (s *server) DeleteAccount(ctx context.Context, req *pb.DeleteAccountRequest) (*pb.BytesResponse, error) {
+// 	return &pb.BytesResponse{}, nil
+// }
+
+// func (s *server) LockAccount(ctx context.Context, req *pb.LockAccountRequest) (*pb.BytesResponse, error) {
+// 	return &pb.BytesResponse{}, nil
+// }
+
+// func (s *server) UnlockAccount(ctx context.Context, req *pb.UnlockAccountRequest) (*pb.BytesResponse, error) {
+// 	return &pb.BytesResponse{}, nil
+// }
+
+// func (s *server) ApproveAddress(ctx context.Context, req *pb.ApproveAddressRequest) (*pb.BytesResponse, error) {
+// 	return &pb.BytesResponse{}, nil
+// }
+
+// func (s *server) RevokeApproval(ctx context.Context, req *pb.RevokeApprovalRequest) (*pb.BoolResponse, error) {
+// 	return &pb.BoolResponse{}, nil
+// }
+
+// func (s *server) Sign(ctx context.Context, req *pb.SignRequest) (*pb.BytesResponse, error) {
+// 	return &pb.BytesResponse{}, nil
+// }
+
+// func (s *server) GetAccount(ctx context.Context, req *pb.AccountIdRequest) (*pb.AccountResponse, error) {
+// 	return &pb.AccountResponse{}, nil
+// }
+
+// func (s *server) IsApproved(ctx context.Context, req *pb.ApproveAddressRequest) (*pb.BoolResponse, error) {
+// 	return &pb.BoolResponse{}, nil
+// }
+
+// func (s *server) IsOwner(ctx context.Context, req *pb.ApproveAddressRequest) (*pb.BoolResponse, error) {
+// 	return &pb.BoolResponse{}, nil
+// }
+
+// func (s *server) IsLocked(ctx context.Context, req *pb.AccountIdRequest) (*pb.BoolResponse, error) {
+// 	return &pb.BoolResponse{}, nil
+// }
+
+// func (s *server) GetLock(ctx context.Context, req *pb.AccountIdRequest) (*pb.TimeLockResponse, error) {
+// 	return &pb.TimeLockResponse{}, nil
+// }
 
 func main() {
 	// Start gRPC server
