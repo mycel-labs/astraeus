fea8698d4886bb9eda4339f3ab31b71dc2fd0d52
peaceandwhisky <pioneerandf@gmail.com>
2024-09-13 00:08:35 +0900

    foundry install in Dockerfile

--- scripts/docker/start-astraeus-api-node.sh
+++ scripts/docker/start-astraeus-api-node.sh
@@ -6,7 +6,10 @@ export RPC_URL=${RPC_URL:-http://host.docker.internal:8545}
 
 echo "Starting astraeus-api--node with arguments: $1"
 
-make run-go 
+forge install
+forge build --via-ir
+forge create --rpc-url $RPC_URL --private-key $PRIVATE_KEY src/solidity/TransferableAccountStore.sol:TransferableAccountStore
 
+make run-go 
 
 
