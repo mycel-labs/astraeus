30ef09e347f289cb3db5f3af4d69926f79d69387
peaceandwhisky <pioneerandf@gmail.com>
2024-09-13 12:14:41 +0900

    createAccount request in docker compose

--- scripts/docker/start-astraeus-api-node.sh
+++ scripts/docker/start-astraeus-api-node.sh
@@ -3,6 +3,8 @@
 export PRIVATE_KEY=${PRIVATE_KEY}
 export RPC_URL=${RPC_URL:-http://host.docker.internal:8545}
 
+export ALICE_PRIVATE_KEY="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+
 echo "Starting astraeus-api--node with arguments: $1"
 
 forge install
@@ -15,3 +17,22 @@ echo "Deployed to ${TA_STORE_CONTRACT_ADDRESS}"
 make run-go &
 
 
+sleep 120
+
+# Run the generating TimedSignature scripts and capture its output
+go_output=$(go run scripts/utils/generate_timed_signature/main.go 2000000000 $ALICE_PRIVATE_KEY)
+
+# Extract the necessary information from the Go script output
+address=$(echo "$go_output" | grep "Address:" | awk '{print $2}')
+message_hash=$(echo "$go_output" | grep "Message Hash:" | awk '{print $3}')
+signature=$(echo "$go_output" | grep "Signature:" | awk '{print $2}')
+
+# Send a POST request about creating a account to the API server
+curl -X POST http://localhost:8080/v1/accounts -d '{
+  "proof": {
+    "validFor": 2000000000,
+    "messageHash": "'"$message_hash"'",
+    "signature": "'"$signature"'",
+    "signer": "'"$address"'"
+  }
+}'
