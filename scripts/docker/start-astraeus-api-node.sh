e22283d71cd00e935af91ef73ef388fb6fa83e48
peaceandwhisky <pioneerandf@gmail.com>
2024-09-13 11:53:08 +0900

    make run-go with start script

--- scripts/docker/start-astraeus-api-node.sh
+++ scripts/docker/start-astraeus-api-node.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-export TA_STORE_CONTRACT_ADDRESS=${TA_STORE_CONTRACT_ADDRESS}
 export PRIVATE_KEY=${PRIVATE_KEY}
 export RPC_URL=${RPC_URL:-http://host.docker.internal:8545}
 
@@ -8,8 +7,11 @@ echo "Starting astraeus-api--node with arguments: $1"
 
 forge install
 forge build --via-ir
-forge create --rpc-url $RPC_URL --private-key $PRIVATE_KEY src/solidity/TransferableAccountStore.sol:TransferableAccountStore
+output=$(forge create --rpc-url $RPC_URL --private-key $PRIVATE_KEY src/solidity/TransferableAccountStore.sol:TransferableAccountStore)
+contract_address=$(echo "$output" | grep "Deployed to:" | awk '{print $3}')
+export TA_STORE_CONTRACT_ADDRESS=$contract_address
+echo "Deployed to ${TA_STORE_CONTRACT_ADDRESS}"
 
-make run-go 
+make run-go &
 
 
