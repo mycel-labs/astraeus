560bc8a1d900867d76ecbbc966f81c8e8520fdd1
foxytanuki <biz@foxytanuki.dev>
2024-10-16 15:29:11 +0900

    chore: remove unused import from contract test

--- test/TransferableAccountStore.t.sol
+++ test/TransferableAccountStore.t.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "forge-std/console.sol";
 import "suave-std/Test.sol";
 import "suave-std/suavelib/Suave.sol";
 import "../src/solidity/TransferableAccountStore.sol";
