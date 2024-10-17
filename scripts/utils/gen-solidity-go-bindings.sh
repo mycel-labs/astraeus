a20acbb29f1a779af5934e3ec658979eb6cd35ff
foxytanuki <biz@foxytanuki.dev>
2024-10-17 18:39:06 +0900

    tweak: location of transferable_account_store.go

--- scripts/utils/gen-solidity-go-bindings.sh
+++ scripts/utils/gen-solidity-go-bindings.sh
@@ -2,5 +2,5 @@
 
 abigen --abi=out/TransferableAccountStore.sol/TransferableAccountStore.abi.json \
 	--bin=out/TransferableAccountStore.sol/TransferableAccountStore.bin \
-	--pkg=contract \
-	--out=src/go/contract/transferable_account_store.go
+	--pkg=transferableaccountstore \
+	--out=src/go/contract/transferable_account_store/transferable_account_store.go
